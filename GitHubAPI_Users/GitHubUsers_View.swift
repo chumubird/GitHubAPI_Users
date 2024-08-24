d39843a468aa520fd3ee65a5066e82ed2f72272b
judas <churoo0418@gmail.com>
2024-08-24 20:16:49 +0900

    [View] setting done

--- GitHubAPI_Users/GitHubUsers_View.swift
+++ GitHubAPI_Users/GitHubUsers_View.swift
@@ -1,15 +1,61 @@
 import SwiftUI
 //start
 struct GitHubUsers_View : View {
+    
+    @StateObject var viewModel = GitHubUsers_ViewModel()
+    @State var inputText : String = ""
+    
+    
+    
     var body: some View {
         //        Text("GitHubUsers_API")
         
         NavigationView{
             VStack{
                 
-                TextField("user ", text: <#T##Binding<String>#>)
+                HStack{
+                    TextField("user name... ", text: $inputText)
+                        .padding()
+                        .frame(width: 250 , height: 50)
+                        .border(Color.black, width: 3)
+                        .padding()
+                        Button(action: {
+                            
+                            print("Search : \(inputText)")
+                            
+                        }, label: {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.black, lineWidth: 3)
+                                .frame(width: 70 ,height: 50)
+                                .overlay {
+                                    Text("Search")
+                                        .font(.system(size: 15))
+                                        .fontWeight(.black)
+                                        .foregroundStyle(.black)
+                                }
+                        })
+                        .disabled( inputText.isEmpty ? true : false)
+                    }
                 
-                
+                List(viewModel.users) { user in
+                    HStack {
+                        AsyncImage(url: URL(string: user.avatar_url)) { image in
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        VStack(alignment: .leading) {
+                            Text(user.login)
+                                .font(.headline)
+                            Text(user.html_url)
+                                .font(.subheadline)
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
                 
             }
             .navigationTitle("Github Users")
--- GitHubAPI_Users/GitHubUsers_ViewModel.swift
+++ GitHubAPI_Users/GitHubUsers_ViewModel.swift
@@ -3,5 +3,9 @@ import SwiftUI
 import Combine
 
 class GitHubUsers_ViewModel : ObservableObject {
+    @Published var users : [GitHubUsers_Model] = []
+    
+    
+    
     
 }
