e541edf136d32895c594cb8903725acb4b940b66
judas <churoo0418@gmail.com>
2024-08-24 19:53:24 +0900

    [Model] set

--- GitHubAPI_Users/GitHubUsers_Model.swift
+++ GitHubAPI_Users/GitHubUsers_Model.swift
@@ -2,6 +2,8 @@ import Foundation
 
 struct GitHubUsers_Model : Codable, Identifiable {
     let id: Int
+    let name: String?
+
       let login: String
       let avatar_url: String
       let html_url: String
--- GitHubAPI_Users/GitHubUsers_View.swift
+++ GitHubAPI_Users/GitHubUsers_View.swift
@@ -2,9 +2,23 @@ import SwiftUI
 //start
 struct GitHubUsers_View : View {
     var body: some View {
-        Text("GitHubUsers_API")
+        //        Text("GitHubUsers_API")
+        
+        NavigationView{
+            VStack{
+                
+                TextField("user ", text: <#T##Binding<String>#>)
+                
+                
+                
+            }
+            .navigationTitle("Github Users")
+            
+            
+        }
     }
 }
+
 
 #Preview {
     GitHubUsers_View()
